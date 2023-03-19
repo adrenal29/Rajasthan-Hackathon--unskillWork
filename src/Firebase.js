8fc08aadec4b9d7e297c4c0b008221dc297d1ffe
Mohit Sharma <mohit29code@gmail.com>
2023-03-19 19:49:22 +0530

    initial commit

---
+++ src/Firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD6IxgKBtkcNu8VrXlFPPgSokVYmgvBsZ4",
+    authDomain: "unskillwork.firebaseapp.com",
+    projectId: "unskillwork",
+    storageBucket: "unskillwork.appspot.com",
+    messagingSenderId: "469278472573",
+    appId: "1:469278472573:web:b575ff7b18a11d93d4f414"
+};
+
+firebase.default.initializeApp(firebaseConfig);
+const auth=firebase.default.auth();
+export {auth , firebase};
